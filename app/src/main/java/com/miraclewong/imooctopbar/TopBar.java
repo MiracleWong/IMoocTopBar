6da11b9c19f289aec524cf72d659f2de35a100b8
MiracleWong <cfwr1991@126.com>
2015-04-17 16:01:48 +0800

    添加自定义的atts.xml

---
+++ app/src/main/java/com/miraclewong/imooctopbar/TopBar.java
@@ -0,0 +1,15 @@
+package com.miraclewong.imooctopbar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by MiracleWong on 2015/4/17.
+ */
+public class TopBar extends RelativeLayout{
+    public TopBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+    }
+}

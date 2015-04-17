e753a33be57d470dffeeb85c36208f6b7856fffb
MiracleWong <cfwr1991@126.com>
2015-04-17 16:22:29 +0800

    自定义的View
    获取自定义的属性到ta中，并从ta中取出，设置自定义的空间，并通过addView()的方法加入到View中

--- app/src/main/java/com/miraclewong/imooctopbar/TopBar.java
+++ app/src/main/java/com/miraclewong/imooctopbar/TopBar.java
@@ -1,15 +1,92 @@
 package com.miraclewong.imooctopbar;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by MiracleWong on 2015/4/17.
  */
 public class TopBar extends RelativeLayout{
+
+    private Button leftButton;
+    private Button rightButton;
+    private TextView tvTitle;
+
+    private int leftTextColor;
+    private Drawable leftBackground;
+    private String leftText;
+
+    private int rightTextColor;
+    private Drawable rightBackground;
+    private String rightText;
+
+    private float titleTextSize;
+    private int titleTextColor;
+    private String titleText;
+
+    // 布局参数
+    private LayoutParams leftParams;
+    private LayoutParams rightParams;
+    private LayoutParams titleParams;
+
     public TopBar(Context context, AttributeSet attrs) {
         super(context, attrs);
+
+        //获取定义的所有值的映射
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.Topbar);
+
+        leftTextColor = ta.getColor(R.styleable.Topbar_leftTextColor, 0);
+        leftBackground = ta.getDrawable(R.styleable.Topbar_leftBackground);
+        leftText = ta.getString(R.styleable.Topbar_leftText);
+
+        rightTextColor = ta.getColor(R.styleable.Topbar_rightTextColor, 0);
+        rightBackground = ta.getDrawable(R.styleable.Topbar_rightBackground);
+        rightText = ta.getString(R.styleable.Topbar_rightText);
+
+        titleTextSize = ta.getDimension(R.styleable.Topbar_titleTextSize, 0);
+        titleTextColor = ta.getColor(R.styleable.Topbar_titleTextColor, 0);
+        titleText = ta.getString(R.styleable.Topbar_titleText);
+
+        ta.recycle();           //回收
+
+        // 实例化已经定义的Button和TextView
+        leftButton = new Button(context);
+        rightButton = new Button(context);
+        tvTitle = new TextView(context);
+
+        leftButton.setTextColor(leftTextColor);
+        leftButton.setBackground(leftBackground);
+        leftButton.setText(leftText);
+
+        rightButton.setTextColor(rightTextColor);
+        rightButton.setBackground(rightBackground);
+        rightButton.setText(rightText);
+
+        tvTitle.setTextColor(titleTextColor);
+        tvTitle.setTextSize(titleTextSize);
+        tvTitle.setText(titleText);
+        tvTitle.setGravity(Gravity.CENTER);     //居中显示
+
+        setBackgroundColor(0xFFF59563);         //设置背景颜色
+
+        leftParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT);   //传入width和height
+        leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, TRUE);         //左对齐
+        addView(leftButton,leftParams);                                     //将左Button加入View中
+
+        rightParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, TRUE);
+        addView(rightButton, rightParams);
+
+        titleParams = new LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,LayoutParams.MATCH_PARENT);
+        titleParams.addRule(RelativeLayout.CENTER_IN_PARENT, TRUE);
+        addView(tvTitle,titleParams);
 
     }
 }
